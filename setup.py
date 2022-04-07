c64418af20f23a761d9e80a551e94182b8cf5072
niubiwocao <93188931+niubiwocao@users.noreply.github.com>
2022-04-07 15:42:55 +0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+import os
+from setuptools import setup
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name='yang-compatibility-check',
+    version='0.1',
+    description=('A plugin to check a YANG file compatibility Schema that according huawei or the third-party rules.'),
+    long_description=read('README.md'),
+    author='niubiwocao',
+    author_email='niubiwocao@huawei.com',
+    license='New-style BSD',
+    url='https://github.com/HuaweiDatacomm/yang-compatibility-check',
+    install_requires=['pyang'],
+    include_package_data=True,
+    keywords=['yang', 'compatibility', 'check', 'pyang'],
+    classifiers=[],
+    
+)
